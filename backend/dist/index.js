eaea24e694d114ca722486ca0dc227c2096da4f0
Renato <renato92.hora@gmail.com>
2024-11-21 16:54:06 -0300

    feat: Configuração do ESlint e migração do bacend para ESM

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,14 +1,9 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-const port = 8080;
+import express from 'express'
+const app = express()
+const port = 8080
 app.get('/', (req, res) => {
-    res.send('Backend funcionando');
-});
+  res.send('Backend funcionando')
+})
 app.listen(port, () => {
-    console.log(`Server is running at <http://localhost>:${port}`);
-});
+  console.log(`Server is running at <http://localhost>:${port}`)
+})
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
-const port = 8080;
+const app = express()
+const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Backend funcionando');
-});
+  res.send('Backend funcionando')
+})
 
 app.listen(port, () => {
-  console.log(`Server is running at <http://localhost>:${port}`);
-});
+  console.log(`Server is running at <http://localhost>:${port}`)
+})
