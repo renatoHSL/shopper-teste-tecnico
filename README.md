83282814c7fcff8b47e292aba7ee8deb685ff866
Renato <renato92.hora@gmail.com>
2024-11-22 15:22:46 -0300

    feat: configuração dos Dockerfiles concluída. Iniciado a configuração do docker-compose

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # shopper-teste-tecnico:
 
+ENTRAS TODO:
+Documente no README do projeto que as vulnerabilidades não foram corrigidas por serem irrelevantes no contexto de desenvolvimento.
+
 Fontes usadas:
 
 Iniciando o backed:
@@ -9,6 +12,8 @@ https://www.digitalocean.com/community/tutorials/typescript-new-project
 
 ESlint + Prettier:
 https://dev.to/joylee/eslint-and-prettier-a-guide-to-cleaner-code-5go9
+https://dev.to/otaviopalma/criando-um-projeto-react-com-typescript-eslint-e-prettier-2okg
+https://eslint.org/docs/latest/integrate/nodejs-api
 
 Common.js to ESM:
 https://www.youtube.com/watch?v=utqADmW7-HE
@@ -21,3 +26,9 @@ Docker:
 https://www.freecodecamp.org/portuguese/news/um-guia-para-iniciantes-em-docker-como-criar-sua-primeira-aplicacao-com-o-docker/
 https://docs.docker.com/reference/dockerfile/
 https://www.digitalocean.com/community/tutorials/how-to-build-a-node-js-application-with-docker
+
+https://www.freecodecamp.org/news/how-to-dockerize-a-react-application/
+
+docker-compose:
+https://docs.docker.com/get-started/workshop/08_using_compose/
+https://www.techrepublic.com/article/how-to-build-a-docker-compose-file/
--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,48 +1,11 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
-import { fixupConfigRules } from '@eslint/compat'
 
-import prettierConfig from 'eslint-config-prettier'
-import pluginPrettier from 'eslint-plugin-prettier'
-
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  {
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 2021,
-        sourceType: 'module',
-      },
-      globals: globals.browser,
-    },
-  },
-
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  ...fixupConfigRules(pluginReactConfig),
-
-  {
-    plugins: {
-      prettier: pluginPrettier,
-    },
-  },
-  {
-    settings: {
-      react: { version: 'detect' },
-    },
-  },
-  {
-    rules: {
-      'no-console': 'warn',
-      'no-unused-vars': 'warn',
-      'react/prop-types': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn'],
-
-      ...prettierConfig.rules,
-      'prettier/prettier': 'error',
-    },
-  },
 ]
